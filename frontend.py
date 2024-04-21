304a86ce292aeafd6265b234cc628a98d9984588
Ajan <ajansunil1972@gmail.com>
2024-04-21 06:06:13 +0530

    updated frontend and added rag_llama2 script

--- frontend.py
+++ frontend.py
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
+import streamlit as st
+from rag_llama2 import rag
+
+st.title("Saheli")
+
+question = st.text_input("Hey, how are you?")
+
+if question:
+    response = rag(question)
+    st.markdown(response)
+=======
 from rag import rag
 import streamlit as st
 
@@ -12,3 +24,4 @@ if st.button("Query Documents"):
         answer = process_input(urls,question)
         st.text_area("Answer",value=answer,height=300,disabled=True)
         
+>>>>>>> 988ba2e7162e9d8b22a6c543a46f35cd98e58879
